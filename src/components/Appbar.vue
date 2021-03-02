5368bae3699a1fa84545fad8121788cba2649c8a
Brandon Vargas <brandon_vargas@homedepot.com>
2021-03-02 12:46:06 -0500

    added homepage start

---
+++ src/components/Appbar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="outterBarWrapper">
+        <img src="../assets/astronaut.png" v-bind:style="{justifySelf: 'left', paddingLeft: '20px'}"/>
+        <div class="innerBarWrapper">
+            <div v-bind:style="{alignSelf: 'center'}">
+                About
+            </div>
+            <div v-bind:style="{alignSelf: 'center'}">
+                Work
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Appbar'
+}
+</script>
+<style scoped>
+.outterBarWrapper {
+    display: grid;
+    grid-template-rows: 1fr;
+    grid-template-columns: 1fr 1fr;
+    justify-items: right;
+    padding: 5px;
+    margin-top: 20px;
+}
+.innerBarWrapper {
+    display: grid;
+    grid-template-rows: 1fr;
+    grid-template-columns: 1fr 1fr;
+    justify-content: right;
+    width: 80%;
+}
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Appbar/>
+        <div class="name">
+            <h1>I'm Brandon</h1>
+        </div>
+    </div>
+</template>
+<script>
+import Appbar from '@/components/Appbar.vue'
+export default {
+    name: 'HomePage',
+    components: {
+        Appbar
+    }
+}
+</script>
+<style scoped>
+.name {
+    font-family: 'Cormorant Infant', serif;
+    margin: 50px 20px 20px 20px;
+}
+</style>
